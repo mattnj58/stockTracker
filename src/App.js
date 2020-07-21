90a9462c016fd9416b624a8a46d75ed2a637ec5a
mattnj58 <mattnj58@gmail.com>
2020-07-21 13:53:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Portfolio from './app_components/portfolio.component';
+import Search from './app_components/search.componenet';
+// import Dashboard from './app_components/dashboard.component';
+
+require('dotenv').config();
+//import Search from './app_components/search.componenet';
+
+class App extends Component {
+    state = {
+      connectionError: false
+  }
+
+  render(){
+    return(
+      <Router>
+        <div className = "App">
+          {/* <div className = "App-header">
+            <Link to='/'>Dashboard</Link>
+            <Link to='/Search'>Search</Link>
+          </div> */}
+          {/* <Switch>
+            <Route exact path='/'></Route>
+            <Route exact path='/Search' component={Search}></Route>
+          </Switch> */}
+          <Portfolio/>
+        </div>
+      </Router>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/app_components/portfolio.component.jsx
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+const stocksURL = 'wss://ws.finnhub.io?token=bs3tkvnrh5rbsfggfo6g';
+// const tiingo='wss://api.tiingo.com/iex'
+
+// var subscribe ={
+//     'eventName':'subscribe', 
+//     'authorization': 'fc3ff2cc4162c09ab83de682f898be17103b1415', 
+//     'eventData':{
+//         'thresholdLevel':'5',
+//         'tickers':['MSFT']
+//     }
+// }
+
+class Portfolio extends Component{
+
+
+    constructor(props){
+        super(props)
+        this.state = {
+            symbols: ['MSFT','TSLA'],
+            symbol:[],
+            price: [],
+            msftPrices:[],
+            tslaPrices: [],
+            messages: []
+        }
+    }
+
+    componentDidMount = () => {
+        this.connection = new WebSocket(stocksURL);
+        var sym="";
+        var p=0;
+
+        this.connection.onmessage = evt => {
+            var packet = JSON.parse(evt.data);
+            console.log(packet)
+            if(packet.type === "trade"){
+                sym = packet.data[0].s;
+                p = packet.data[0].p;
+                if(sym==="MSFT"){
+                    this.setState({
+                        msftPrices: this.state.msftPrices.concat([p])
+                    })
+                } else if(sym==="TSLA"){
+                    this.setState({
+                        tslaPrices: this.state.tslaPrices.concat([p])
+                    })
+                }
+
+                // console.log(this.state.msftPrices)
+            } else if (packet.type!=="trade"){
+                // sym = this.state.symbol;
+                // p = this.state.msftPrices.split(-1);
+
+            }
+
+            // console.log(p)
+
+            this.setState({
+                symbol: sym,
+                price: p,
+                messages: this.state.messages.concat([evt.data])
+            })
+        };
+
+        setInterval( _ => {
+            if(!this.isOpen(this.connection)) return;
+            // this.connection.send(JSON.stringify(subscribe));
+            this.connection.send(JSON.stringify({'type':'subscribe', 'symbol':'TSLA'}))
+            // this.connection.send (JSON.stringify({'type':'subscribe', 'symbol': 'MSFT'}))
+        },1000)
+    }
+
+    isOpen(ws){
+        return ws.readyState === ws.OPEN
+    }
+
+    render(){
+        return(
+            <div>
+                {/* <ul>{ this.state.messages.slice(-1).map( (msg, idx) => <li key={'msg-' + idx }>{ msg }</li> )}</ul> */}
+                <h1 style={{fontSize: '100px'}}>Dashboard</h1>
+                <h1 style={{fontSize: '100px'}}>{this.state.symbol}</h1>
+                <h2 style={{fontSize: '100px'}}>{ this.state.price }</h2>
+            </div>
+        );
+    }
+}
+
+export default Portfolio;
