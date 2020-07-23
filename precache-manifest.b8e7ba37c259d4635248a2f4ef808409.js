859194d70c11368fb4b01cea2969b23168510f59
mattnj58 <mattnj58@gmail.com>
2020-07-23 11:00:16 -0400

    Updates

---
+++ precache-manifest.b8e7ba37c259d4635248a2f4ef808409.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7739facf34dce2e24afe20868e33352",
+    "url": "/stockTracker/index.html"
+  },
+  {
+    "revision": "21659eff81dc57fe53d8",
+    "url": "/stockTracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "64729409dd9abd9f34af",
+    "url": "/stockTracker/static/css/main.85b3211f.chunk.css"
+  },
+  {
+    "revision": "21659eff81dc57fe53d8",
+    "url": "/stockTracker/static/js/2.22c7b765.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/stockTracker/static/js/2.22c7b765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64729409dd9abd9f34af",
+    "url": "/stockTracker/static/js/main.87e6ed89.chunk.js"
+  },
+  {
+    "revision": "6d098e36d983b2fb82ab",
+    "url": "/stockTracker/static/js/runtime-main.1f263bc6.js"
+  }
+]);
